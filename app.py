09d5e4494e8d68ad207a118415e97201d3497664
Anusree Subash <anusreesubash@gmail.com>
2020-07-26 19:29:50 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,82 @@
+from flask import Flask, jsonify, request, render_template
+
+# Create an app __name__ is a variable that gives each file a unique name
+app = Flask(__name__)
+
+stores = [
+  {
+    'name': 'my-store',
+    'items': [
+      {
+        'name': 'item1',
+        'price': '43'
+
+      }
+    ]
+  }
+]
+
+# request app will understand
+@app.route('/')
+def home():               #method
+  return render_template('index.html')
+
+# post/store/data:(name:)
+@app.route('/store', methods=['POST'])
+def create_store():
+  request_data = request.get_json() # allows to get data from json
+  new_store = {
+    'name': request_data['name'],
+    'items': []
+  }
+  stores.append(new_store)
+  return jsonify(new_store)
+  
+
+# get/store/<string:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+  #iterate over stores
+  # if the store name matches return that
+  # else return error
+  for store in stores:
+    if store['name'] == name:
+      return jsonify(store)
+    
+    return jsonify({'message':'store not found'})
+
+
+# get/store
+@app.route('/store')
+def get_stores():
+  return jsonify({'stores':stores}) # since stores is a list and jasonify reads only dictionaries
+
+
+# post/store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+  for store in stores:
+    if store['name'] == name:
+      request_data = request.get_json()
+      new_item = {
+      'name': request_data['name'],
+      'price': request_data['price']
+      }
+      store['items'].append(new_item)
+      return jsonify(new_item)
+
+    return jsonify({'message': 'store not found'})
+
+
+# get/store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+  for store in stores:
+    if store['name'] == name:
+      return jsonify({'items': store['items']})
+    
+    return jsonify({'message':'store not found'})
+
+
+#tell app to run
+app.run(port=5000)
---
+++ code/app.py
@@ -0,0 +1,61 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
+
+app = Flask(__name__)
+app.secret_key = 'anusree'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
+
+items = []
+
+class Item(Resource):
+  parser = reqparse.RequestParser()
+  parser.add_argument('price',
+    type = float,
+    required = True,
+    help = 'this cannot be left blank'
+    )
+  data = parser.parse_args()
+  @jwt_required() 
+  def get(self, name):
+    item = next(filter(lambda x: x['name'] == name, items), None)
+    return {'item': item}, 200 if item else 404
+
+  def delete(self, name):
+    global items
+    items = list(filter(lambda x: x['name'] == name, items))
+    return {'message': 'item deleted'}
+
+  def put(self, name):
+    
+
+    item = next(filter(lambda x: x['name'] == name,items), None)
+    if item == None:
+      item = {'name': name, 'price': data['price']}
+      items.append(item)
+    else:
+      item.update(data)
+    return item
+
+  def post(self, name):
+
+    if next(filter(lambda x: x['name'] == name, items), None):
+      return {'message': 'This item already exists'.format(name)}
+    data = Item.parser.parse_args()
+    item = {'name': name, 'price': data['price'] }
+    items.append(item)
+    return item, 201
+
+class ItemList(Resource):
+  def get(self):
+    return{'items': items}
+    
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
